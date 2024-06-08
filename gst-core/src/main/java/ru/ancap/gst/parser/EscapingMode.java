17f30b6ac6cd24aef60065974baa48c47b915836
PukPukov <kvasov.les@gmail.com>
2024-06-08 19:06:51 +0500

    better document escaping mode

--- gst-core/src/main/java/ru/ancap/gst/parser/EscapingMode.java
+++ gst-core/src/main/java/ru/ancap/gst/parser/EscapingMode.java
@@ -1,8 +1,23 @@
 package ru.ancap.gst.parser;
 
 public enum EscapingMode {
+
+    /**
+     * Placeholders will look like "\{id}", "{id}" is just text.
+     * <p>
+     * Longer, but support backward compatibility on adding new special constructions.
+     * That way Java works with in-code strings (with exception to base quotes)
+     */
+    UNESCAPED_IS_SIMPLE_TEXT,
     
-    UNESCAPED_IS_SIMPLE_TEXT, // placeholders will look like "\{id}", "{id}" is just text
-    UNESCAPED_IS_PLACEHOLDER  // placeholders will look like "{id}", "\{id}" is text "{id}"
+    /**
+     * Placeholders will look like "{id}", "\{id}" is text "{id}".
+     * <p>
+     * Shorter, but old strings may break when adding new constructions
+     * (for example, you had "red" keyword in some handler, and "blue" 
+     * interpreted as simple text, then if you decided to add "blue" 
+     * keyword all previous strings that used "blue" as text will change.
+     */
+    UNESCAPED_IS_PLACEHOLDER
     
 }
