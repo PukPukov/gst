53212940f07ba2ad7c7296d8df18ff6207294736
PukPukov <kvasov.les@gmail.com>
2024-06-10 02:29:23 +0500

    make all records used in project support "with"-based construction (until java didn't added with keyword)

--- gst-core/src/main/java/ru/ancap/gst/GeneralStringTemplate.java
+++ gst-core/src/main/java/ru/ancap/gst/GeneralStringTemplate.java
@@ -1,5 +1,8 @@
 package ru.ancap.gst;
 
+import lombok.With;
+
 import java.util.Collection;
 
+@With
 public record GeneralStringTemplate(String base, Collection<String> excludedKeys) { }
--- gst-core/src/main/java/ru/ancap/gst/parser/gst_structure/Placeholder.java
+++ gst-core/src/main/java/ru/ancap/gst/parser/gst_structure/Placeholder.java
@@ -1,7 +1,10 @@
 package ru.ancap.gst.parser.gst_structure;
 
+import lombok.With;
+
 import java.util.Optional;
 
+@With
 public record Placeholder(DirectPlaceholderData directData, boolean exclusion, boolean endExpected) implements GSTPart {
     
     public static final Placeholder DUMMY = new Placeholder(new DirectPlaceholderData("foo", Optional.empty(), new Text(0, "\\{foo}")), false, true);
