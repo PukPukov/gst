0727b9d3c8343b3f5db767a9cba2f6bf9e9fbec0
PukPukov <vovanchikputin@vk.com>
2024-06-10 02:29:23 +0500

    make all records used in project support "with"-based construction (until java didn't added with keyword)

--- gst-core/src/main/java/ru/ancap/gst/parser/gst_structure/DirectPlaceholderData.java
+++ gst-core/src/main/java/ru/ancap/gst/parser/gst_structure/DirectPlaceholderData.java
@@ -1,5 +1,8 @@
 package ru.ancap.gst.parser.gst_structure;
 
+import lombok.With;
+
 import java.util.Optional;
 
+@With
 public record DirectPlaceholderData(String key, Optional<String> argument, Text originalPart) { }
--- gst-core/src/main/java/ru/ancap/gst/parser/gst_structure/Text.java
+++ gst-core/src/main/java/ru/ancap/gst/parser/gst_structure/Text.java
@@ -1,6 +1,9 @@
 package ru.ancap.gst.parser.gst_structure;
 
+import lombok.With;
+
 /**
  * @param index first text symbol 0-based index in original string
  */
+@With
 public record Text(int index, String string) implements GSTPart { }
